8ffe7fe3d7c3e93ffc28311d905a04b30525d0c5
nooblette <69203354+nooblette@users.noreply.github.com>
2021-04-13 22:46:12 +0900

    Update ex7_kwsstt.py with COOMMENT

--- ex7_kwsstt.py
+++ ex7_kwsstt.py
@@ -8,20 +8,22 @@ Example 7: 호출어 음성인식 결합 예제
 from __future__ import print_function
 
 import time
-import ex2_getVoice2Text as gv2t
-import ex1_kwstest as kws
+import ex2_getVoice2Text as gv2t  #ex2 Sound To Text
+import ex1_kwstest as kws  #ex1 호출어 인식
 
-def main():
+def main():  # ex1과 ex2를 합침, 호출어를 사용하여 AMK를 호출하고, 음성을 텍스트로 받음
 	#Example7 KWS+STT
+	#KWS -> ex1_kwstest : 호출어를 인식
+	#gv2t -> ex2_getVoice2Text : 기가지니를 호출 한 후 음성으로 데이터 입력
 
 	KWSID = ['기가지니', '지니야', '친구야', '자기야']
 	while 1:
 		recog=kws.test(KWSID[0])
 		if recog == 200:
 			print('KWS Dectected ...\n Start STT...')
-			text = gv2t.getVoice2Text()
-			print('Recognized Text: '+ text)
-			time.sleep(2)
+			text = gv2t.getVoice2Text()  # 음성으로 입력받은 데이터를 텍스트로 변환해서 리턴
+			print('Recognized Text: '+ text)  # 리턴받은 데이터를 화면에 출력
+			time.sleep(2)  # 2초동안 아무 반응 없으면 종료
 			
 		else:
 			print('KWS Not Dectected ...')
