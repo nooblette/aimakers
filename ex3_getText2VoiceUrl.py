3f894dab7ef3947e3c9d1ba96fac813a81492edb
nooblette <69203354+nooblette@users.noreply.github.com>
2021-04-13 15:49:37 +0900

    Update ex3_getText2VoiceUrl.py with COMMENT

--- ex3_getText2VoiceUrl.py
+++ ex3_getText2VoiceUrl.py
@@ -16,16 +16,17 @@ PORT = 4080
 
 
 # TTS : getText2VoiceUrl
-def getText2VoiceUrl(inText):
+def getText2VoiceUrl(inText):  #inText를 출력할 수 있는 url를 출력(출력하는 url은 1분간 유효함)
 
 	channel = grpc.secure_channel('{}:{}'.format(HOST, PORT), UA.getCredentials())
 	stub = gigagenieRPC_pb2_grpc.GigagenieStub(channel)
 
 	message = gigagenieRPC_pb2.reqText()
-	message.lang=0
+	message.lang=0  # 한국어명 lang = 0 
 	message.mode=0
-	message.text=inText
-	response = stub.getText2VoiceUrl(message)
+	message.text=inText  # 변환할 텍스트 입력
+	response = stub.getText2VoiceUrl(message)  # url로 변환
+	# line 24~28 : 사용할 언어를 설정, 텍스트를 입력해 변환
 
 	print ("\n\nresultCd: %d" % (response.resultCd))
 	if response.resultCd == 200:
@@ -36,7 +37,8 @@ def getText2VoiceUrl(inText):
 		print ("Fail: %d" % (response.resultCd)) 
 
 def main():
-	getText2VoiceUrl("안녕하세요. 반갑습니다.")
+	# Text to SPeech(TTS) : 지정된 텍스트를 음성으로 바꾸어 음성을 들을 수 있는 url을 제공
+	getText2VoiceUrl("안녕하세요. 반갑습니다.")  # 변경하고싶은 단어나 문장을 매개변수로 설정
 
 if __name__ == '__main__':
 	main()
