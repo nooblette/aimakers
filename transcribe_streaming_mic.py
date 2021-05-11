ff70b472f40b4878ab3cddc3a10b626b59822907
nooblette <69203354+nooblette@users.noreply.github.com>
2021-05-12 04:13:49 +0900

    STT, TTS example and How to Use
    STT, TTS example source from https://github.com/googleapis
    
    실행하기 위한 명령어 모음(기본 콘솔 창(홈 디렉토리)에서 Env라는 가상환경에서 실행)
    
    - 가상환경으로 접속(성공하면 앞에 (env)가 따라붙음)
    source ~/env/bin/activate
    
    - 샘플 예제 실행하기(뒤에 --lang ko-KR 안붙이면 영어로 실행)
    googlesamples-assistant-pushtotalk --project-id ai-speaker-e1dab --device-model-id ai-speaker-e1dab-readingbird-cjc51y --lang ko-KR
    
    - How to Set Environment Variable in python code
    import os 
    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"제이슨파일경로/제이슨파일명.json"
    
    - TTS 예제 설명
      - quickstart.py : 
            방법 1) 'Hello world' 라는 텍스트를 영어(language code = en-US)로 output.mp3로 출력
            방법 2) '안녕 만나서 반가워' 라는 텍스트를 한국어(language code = ko-KR)로 output.mp3로 출력
    
      - audio_profile.py : 
            python audio_profile.py --output filename.확장자 --text "텍스트 내용" --effects_profile_id 프로필아이디
                ex) python audio_profile.py --output filename.mp3 --text "Hello ReadingBird" --effects_profile_id telephony-class-application
            quickstart를 조금 더 편하게 쓸 수 있도록 바꿔준 것
            (어떤 형식으로 파일을 저장하고 어떤식으로 쓸지 등..)
            --effects_profile_id : 사운드를 각각의 하드웨어에 최적해서 출력해줌
            (AI Speaker는 medium-bluetooth-speaker-class-device 스마트 홈 스피커(구글홈)
              or small-bluetooth-speaker-class-device 소형 가정용 스피커(구글 홈 미니)
              or telephony-class-application 대화형 음성 응답(IVR) 시스)
    
    
      - synthesize_text.py : 
            방법 1) python synthesize_text.py --text "텍스트 내용" 으로 실행
                   실행파일의 매개변수로 전달한 문자열 텍스트를 사운드로 변환해줌
            방법 2) python synthesize_text.py --ssml "<speak>text contents</speak>" 으로 실행
                   실행파일의 매개변수로 전달한 텍스트를 마크업해서 사운드로 변환해줌
                   (텍스트 내용을 예쁘게 꾸미는것(천천히, 빠르게, 몇 템포 쉬었다가, 이러한 발음으로 등..)
    
      - synthesize_file.py :
            방법 1) python synthesize_file.py --text textfilename.txt
            방법 2) python synthesize_file.py --ssml ssmlfilename.ssml

---
+++ transcribe_streaming_mic.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Speech API sample application using the streaming API.
+
+NOTE: This module requires the additional dependency `pyaudio`. To install
+using pip:
+
+    pip install pyaudio
+
+Example usage:
+    python transcribe_streaming_mic.py
+"""
+
+# [START speech_transcribe_streaming_mic]
+from __future__ import division
+
+import re
+import sys
+
+from google.cloud import speech
+
+import pyaudio
+from six.moves import queue
+from threading import Thread  # added by mh
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+        self.isPause = False  # added by mh
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def pause(self):  # for pasue, added by mh
+        if self.isPause == False:
+            self.isPause = True
+ 
+    def resume(self):  # for resume, added by mh
+        if self.isPause == True:
+            self.isPause = False
+
+    def status(self):
+        return self.isPause
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        if self.isPause == False:  # added by mh
+            self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+# [END audio_stream]
+
+class Gspeech(Thread):  # class added by mh
+    def __init__(self):
+        Thread.__init__(self)
+
+        self.language_code = 'ko-KR'  # a BCP-47 langugae tag
+
+        self._buff = queue.Queue()
+        self.client = speech.SpeechClient()
+        self.config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=RATE,
+            language_code=self.language_code)
+        self.streaming_config = speech.StreamingRecognitionConfig(
+            config=self.config, 
+            interim_results=True)
+
+        self.mic = None
+        self.status = True
+
+        self.daemon = True
+        self.start()
+
+    def __exit__(self):
+        self._buff.put(None)
+
+    def run(self):
+        with MicrophoneStream(RATE, CHUNK) as stream:
+            self.mic = stream
+            audio_generator = stream.generator()
+            requests = (types.StreamingRecognizeRequest(audio_content=content) for content in audio_generator)
+
+            responses = self.client.streaming_recognize(self.streaming_config, requests)
+
+            # Now, put the transcription responses to use.
+            self.listen_print_loop(responses, stream)
+        self._buff.put(None)
+        self.status = False
+
+    def pauseMic(self):
+        if self.mic is not None:
+            self.mic.pause()
+
+    def resumeMic(self):
+        if self.mic is not None:
+            self.mic.resume()
+
+    # 인식된 텍스트 가져가기
+    def getText(self, block = True):
+        return self._buff.get(block=block)
+
+    # 음성 인식 처리 루틴
+    def listen_print_loop(self, responses, mic):
+        num_chars_printed = 0
+        try:
+            for response in responses:
+                if not response.results:
+                    continue
+
+                # The `results` list is consecutive. For streaming, we only care about
+                # the first result being considered, since once it's `is_final`, it
+                # moves on to considering the next utterance.
+                result = response.results[0]
+                if not result.alternatives:
+                    continue
+
+                # Display the transcription of the top alternative.
+                transcript = result.alternatives[0].transcript
+
+                # Display interim results, but with a carriage return at the end of the
+                # line, so subsequent lines will overwrite them.
+                #
+                # If the previous result was longer than this one, we need to print
+                # some extra spaces to overwrite the previous result
+                overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+                if not result.is_final:
+                    sys.stdout.write(transcript + overwrite_chars + "\r")
+                    sys.stdout.flush()
+                    num_chars_printed = len(transcript)
+
+                else:
+                    self._buff.put(transcript + overwrite_chars)
+                    num_chars_printed = 0
+        except:
+            return
+'''
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                #if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                if re.search(r"\b(잘가|잘 가)\b", transcript, re.I):
+                print("Exiting..")
+                break
+'''
+
+def main():
+    gsp = Gspeech()
+    while True:
+        stt = gsp.getText()
+        if stt in None:
+            break
+        print(stt)
+        time.sleep(0.01)
+        if ('잘 가|잘가' in stt):
+            break
+
+
+'''
+def main():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    #language_code = "en-US"  # a BCP-47 language tag
+    language_code = 'ko-KR'  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code,
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses)
+'''
+
+if __name__ == "__main__":
+    main()
+# [END speech_transcribe_streaming_mic]
